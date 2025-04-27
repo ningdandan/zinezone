34eea340849e9b2c9f83336a42db492a5f8e42c9
ningdandan <einooo520@gmail.com>
2025-04-26 19:18:41 -0700

    Initial Commit

---
+++ ZineZone/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ZineZone
+//
+//  Created by 张薴丹 on 4/26/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ ZineZoneTests/ZineZoneTests.swift
@@ -0,0 +1,17 @@
+//
+//  ZineZoneTests.swift
+//  ZineZoneTests
+//
+//  Created by 张薴丹 on 4/26/25.
+//
+
+import Testing
+@testable import ZineZone
+
+struct ZineZoneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
